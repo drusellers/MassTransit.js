d8f7fcaa1bf456e0d6117d800928df27816d97f9
Brian Mavity <brian@brianmavity.com>
2011-01-31 14:35:01 -0600

    Removed eventedsocket library because it wasn't needed and styled active/inactive bus

--- samples/socket/dashboard/server.js
+++ samples/socket/dashboard/server.js
@@ -1,6 +1,5 @@
 var http = require('http'),  
     io = require('socket.io'),
-    evented = require('eventedsocket'),
     connect = require('connect'),
     fs = require('fs'),
     config = require('./config'),
@@ -12,28 +11,16 @@ var http = require('http'),
     socket = io.listen(server); 
 
 server.listen(8080);
-var clients = [];
-socket.on('connection', function(client) { 
-  var eClient = evented.client(client);
-  eClient.on('message', function() {
-    console.log('message');
-  });
 
-  eClient.on('disconnect', function() {
+socket.on('connection', function(client) { 
+  console.log('client: ', client);
+  client.on('disconnect', function() {
     console.log('disconnect');
   });
-
-  //eClient.emit('a:newSubscription', { hi: 'from me' });
-  clients.push(eClient);
 }); 
 
 bus.ready(config, function() {
   bus.subscribe('Diagnostics', function(m) {
-    console.log(m);
-    clients.forEach(function(client) {
-      client.emit(m.evt, m);
-    //socket.broadcast('a:busCreated', m);
-    //socket.broadcast('a:newSubscription', m);
-    });
+    socket.broadcast(m);
   });
 });
